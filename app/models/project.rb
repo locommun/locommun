1fe37350670fd67308d29fa23ac1ef5f60ab46e9
Cenk Gündoğan <cnkgndgn@gmail.com>
2013-06-01 16:12:39 +0200

    funny commit situation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,17 +1,20 @@
 class Project < ActiveRecord::Base
    include Rails.application.routes.url_helpers
-  attr_accessible :description, :gmaps, :latitude, :longitude, :title
-  
+  attr_accessible :description, :gmaps, :latitude, :longitude, :title, :image
+
   belongs_to :user
-  
+  has_many :comments
+
+  has_attached_file :image, :styles => { :medium => "520x360>", :thumb => "260x180#" , :mini => "130x90#"}
+
   validates :title, :length => {:minimum => 3, :maximum => 30}
   validates_numericality_of :latitude, {:minimum => 0, :maximum => 90}
   validates_numericality_of :longitude, {:minimum => -180, :maximum => 180}
-  
+
   acts_as_gmappable :process_geocoding => false
-  
+
   reverse_geocoded_by :latitude, :longitude
-  
+
   def gmaps4rails_address
     ""
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Locommun::Application.routes.draw do
-  
+
   resources :projects
   resources :sponsor_finder
-
   devise_for :users
 
-  
+
   root :to => 'blogit/posts#index'
 
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   mount Blogit::Engine => "/blog"
-  
+
 end
